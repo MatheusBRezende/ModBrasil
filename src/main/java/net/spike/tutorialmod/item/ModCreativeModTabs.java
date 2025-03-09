e4b6bde96bbd423652ef09aa63a1316648533104
Matheus <mbazarello@gmail.com>
2025-03-09 13:40:03 -0300

    Changes

--- src/main/java/net/spike/tutorialmod/item/ModCreativeModTabs.java
+++ src/main/java/net/spike/tutorialmod/item/ModCreativeModTabs.java
@@ -8,18 +8,17 @@ import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.RegistryObject;
 import net.spike.tutorialmod.TutorialMod;
-import net.spike.tutorialmod.block.ModBlocks;
+
 
 public class ModCreativeModTabs{
     public static final DeferredRegister<CreativeModeTab> CREATIVE_MODE_TABS =
             DeferredRegister.create(Registries.CREATIVE_MODE_TAB, TutorialMod.MODID);
 
     public static final RegistryObject<CreativeModeTab> TUTORIAL_TAB = CREATIVE_MODE_TABS.register("tutorial_tab",
-            () -> CreativeModeTab.builder(). icon(()-> new ItemStack(ModItems.CERVEJA.get()))
+            () -> CreativeModeTab.builder(). icon(()-> new ItemStack(ModItems.DINHEIRO_1.get()))
 
                     .title(Component.translatable("creativetab.tutorial_tab"))
                     .displayItems((itemDisplayParameters, output) -> {
-                        output.accept(ModItems.CERVEJA.get());
                         output.accept(ModItems.DINHEIRO_1.get());
                         output.accept(ModItems.DINHEIRO_2.get());
                         output.accept(ModItems.DINHEIRO_5.get());
--- src/main/java/net/spike/tutorialmod/item/ModItems.java
+++ src/main/java/net/spike/tutorialmod/item/ModItems.java
@@ -15,40 +15,31 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS =
             DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MODID);
 
-    public static final RegistryObject<Item> CERVEJA = ITEMS.register("cerveja",
-            () -> new CervejaItem(new Item.Properties()
-                    .food(new FoodProperties.Builder()
-                            .nutrition(2) // Quantidade de fome recuperada
-                            .saturationMod(0.6f) // Nível de saturação
-                            .effect(() -> new MobEffectInstance(MobEffects.CONFUSION, 300, 0), 0.5f) // Efeito de náusea
-                            .effect(() -> new MobEffectInstance(MobEffects.WEAKNESS, 300, 0), 0.5f) // Efeito de fraqueza
-                            .alwaysEat() // Permite que o item seja consumido mesmo com a barra de fome cheia
-                            .build())));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
     }
-    public static final RegistryObject<Item> DINHEIRO_1 = ITEMS.register("dinheiro_1",
+    public static final RegistryObject<Item> DINHEIRO_1 = ITEMS.register("dinheiro1",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> DINHEIRO_2 = ITEMS.register("dinheiro_2",
+    public static final RegistryObject<Item> DINHEIRO_2 = ITEMS.register("dinheiro2",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> DINHEIRO_5 = ITEMS.register("dinheiro_5",
+    public static final RegistryObject<Item> DINHEIRO_5 = ITEMS.register("dinheiro5",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> DINHEIRO_10 = ITEMS.register("dinheiro_10",
+    public static final RegistryObject<Item> DINHEIRO_10 = ITEMS.register("dinheiro10",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> DINHEIRO_20 = ITEMS.register("dinheiro_20",
+    public static final RegistryObject<Item> DINHEIRO_20 = ITEMS.register("dinheiro20",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> DINHEIRO_50 = ITEMS.register("dinheiro_50",
+    public static final RegistryObject<Item> DINHEIRO_50 = ITEMS.register("dinheiro50",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> DINHEIRO_100 = ITEMS.register("dinheiro_100",
+    public static final RegistryObject<Item> DINHEIRO_100 = ITEMS.register("dinheiro100",
             () -> new Item(new Item.Properties()));
 
-    public static final RegistryObject<Item> DINHEIRO_200 = ITEMS.register("dinheiro_200",
+    public static final RegistryObject<Item> DINHEIRO_200 = ITEMS.register("dinheiro200",
             () -> new Item(new Item.Properties()));
 }
